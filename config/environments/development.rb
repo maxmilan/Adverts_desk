cc4b087ef0c95c71a3bb32f80715942ec0cf7287
maxmilan <max231194@gmail.com>
2014-10-02 00:46:42 +0300

    Fixed some bugs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.action_view[:debug_rjs] = true
+  #config.action_view[:debug_rjs] = true
 
   # Do not eager load code on boot.
   config.eager_load = false
--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -2,6 +2,17 @@
 SimpleForm.setup do |config|
   config.button_class = 'btn btn-success'
 
+  config.wrappers :bootstrap, tag: 'div', class: 'control-group', error_class: 'error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label
+    b.wrapper tag: 'div', class: 'controls' do |ba|
+      ba.use :input
+      ba.use :error, wrap_with: {tag: 'span', class: 'help-inline'}
+      ba.use :hint, wrap_with: {tag: 'p', class: 'help-block'}
+    end
+  end
+
   config.wrappers :vertical_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
