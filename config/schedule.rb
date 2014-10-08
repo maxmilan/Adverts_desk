9b9629bf31a3e14b31e95d3b7703843bfc73db04
maxmilan <max231194@gmail.com>
2014-10-08 20:41:51 +0300

    Removed empty files

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,18 +1,3 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
 every 10.minutes do
